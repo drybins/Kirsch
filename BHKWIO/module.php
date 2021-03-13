679ddf0d119ff293447dbdf1c9a0dc821fa616a4
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-03-13 17:22:55 +0100

    Add files via upload

---
+++ BHKWIO/module.php
@@ -0,0 +1,33 @@
+<?php
+	class BHKWIO extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		public function ForwardData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			IPS_LogMessage("IO FRWD", utf8_decode($data->Buffer));
+		}
+
+		public function Send(string $Text)
+		{
+			$this->SendDataToChildren(json_encode(Array("DataID" => "{683476B1-08B4-682B-EF58-B892247FC2E1}", "Buffer" => $Text)));
+		}
+
+	}

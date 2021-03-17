5e48049b62c0269e15d509a4d78ba8ea2e19dfbf
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-03-17 16:07:30 +0100

    Update module.php

--- BHKWIO/module.php
+++ BHKWIO/module.php
@@ -6,8 +6,8 @@
 			//Never delete this line!
 			parent::Create();
 			
-			IPS_LogMessage("Parent ID: ",$this->GetParentId());
-			IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
+			IPS_LogMessage("Parent ID: ","Dierk");
+			//IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
 		}
 
 		public function Destroy()
@@ -21,8 +21,10 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 			
-			IPS_LogMessage("Parent ID: ",$this->GetParentId());
-			IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
+			IPS_LogMessage("Parent ID: ","Dierk");
+
+			//IPS_LogMessage("Parent ID: ",$this->GetParentId());
+			//IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
 		}
 
 		public function ForwardData($JSONString)

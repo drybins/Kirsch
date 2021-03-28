ef2a69314fa34bcbd212f37f5226d5ffb321d7a7
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-03-28 16:44:05 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -125,12 +125,12 @@
 			//IPS_SetEventCondition($eventID,$this->GetIDForIdent('Aussentemperatur'));
 			//IPS_SetEventTrigger($eventID,0,$this->GetIDForIdent('Aussentemperatur'));
 			//IPS_SetEventScript($eventID, "$this->VorlaufSoll()");
-			//
+			/*
 			$CatID = IPS_CreateCategory();       // Kategorie anlegen
 			IPS_SetName($CatID, "statePP");	
 			$this->RegisterVariableInteger("LPR", "Letztes Paket empfangen", " ~UnixTimestamp", 900);
 			IPS_SetParent($this->GetIDForIdent('LPR'),$CatID);
-			//
+			*/
 			$this->ConnectParent("{33B9B2D7-6BC5-1CF6-A86F-E76622A7FFB7}");
 			
 		}
@@ -168,7 +168,7 @@
 			switch ($cmd)
 			{
 				case "statePP":
-					//SetValue($this->GetIDForIdent("LPR"), date("d.m.y H:i"));
+					SetValue($this->GetIDForIdent("LPR"), date("d.m.y H:i"));
 					$this->statePP($data);
 					break;
 				case "statePower":

25a1d8b85fb0ef173bc16dfc9b99253b22a49c00
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-03-28 17:27:54 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -168,7 +168,7 @@
 			switch ($cmd)
 			{
 				case "statePP":
-					SetValue($this->GetIDForIdent("LPR"), date("d.m.y H:i"));
+					SetValue(GetIDForIdent("LPR"), date("d.m.y H:i"));
 					$this->statePP($data);
 					break;
 				case "statePower":

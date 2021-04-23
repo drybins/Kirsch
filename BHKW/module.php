0128db9a60ddfa57cd4453fcb6ef989adac0e0a4
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-04-23 14:58:56 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -220,6 +220,7 @@ if (!defined('VorlaufSollminus20')) {
 		private function errors($data)
 		{
 			$arr = [];
+			$Datumalt ="";
 			$array = json_decode(json_encode(simplexml_load_string($data)),true);
 			if(!empty($array))
 			{

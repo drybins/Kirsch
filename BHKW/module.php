257a9b4bd62914ec528c5ef5a4c5da037e8e5f01
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-04-27 14:56:23 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -163,7 +163,8 @@ if (!defined('VorlaufSollminus20')) {
 			
 			//$CatID = IPS_CreateCategory();       // Kategorie anlegen
 			//IPS_SetName($CatID, "Test1");	
-			$instance = IPS_GetInstance($this->InstanceID);
+			//$instance = IPS_GetInstance($this->InstanceID);
+			//$KategorieID = @IPS_GetCategoryIDByName("statePP", $ÜbergeordneteID);
 			//$instance = IPS_GetCategoryIDByName ("Kirsch BHKW", 0)
 			//IPS_LogMessage("BHKW ID", $instance);
 			//$this->RegisterVariableInteger("LPR", "Letztes Paket empfangen", " ~UnixTimestamp", 900);

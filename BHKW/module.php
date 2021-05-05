fbae829ce82cd5573742fb92ecd8dd18e5df3874
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-05 23:44:03 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -675,13 +675,13 @@ if (!defined('VorlaufSollminus20')) {
 			$VorlaufTempStep = $VorlaufTempDiff/40;
 			$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
 			//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
-			If($AußenTemperatur > 3)
-			{
+			//If($AußenTemperatur > 3)
+			//{
     				If(($time >= "22:30")or($time <= "05:00"))
     				{
         			$VorlaufSoll = $VorlaufSoll - 5;
     				}
-			}
+			//}
 			IPS_LogMessage("$VorlaufTempStep",$VorlaufTempStep);
 			IPS_LogMessage("AußentemperaturT",$VorlaufSoll);
 			IPS_LogMessage("Außentemperatur", $this->GetIDForIdent("T1"));

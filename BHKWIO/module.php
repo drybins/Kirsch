0719bc8630f83ab184fd37ed8de09cfa8e426b45
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-06 16:36:27 +0200

    Add files via upload

--- BHKWIO/module.php
+++ BHKWIO/module.php
@@ -5,8 +5,10 @@
 		{
 			//Never delete this line!
 			parent::Create();
-			
-			IPS_LogMessage("Parent ID: ","dierk");
+			$this->RegisterPropertyString('bhkw_ip', "192.168.178.111");
+			$this->RegisterPropertyInteger('port', 12004);
+        		
+			//IPS_LogMessage("Parent ID: ","Dierk");
 			//IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
 		}
 
@@ -20,7 +22,9 @@
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
-			IPS_LogMessage("Parent ID: ","Dierk");
+			
+			//IPS_LogMessage("Parent ID: ","Dierk");
+
 			//IPS_LogMessage("Parent ID: ",$this->GetParentId());
 			//IPS_LogMessage("BHKWIO ID: ", $this->InstanceID));
 		}
@@ -29,12 +33,33 @@
 		{
 			$data = json_decode($JSONString);
 			IPS_LogMessage("IO FRWD", utf8_decode($data->Buffer));
-			IPS_LogMessage("Parent ID: ","Dierk");
 		}
 
 		public function Send(string $Text)
 		{
 			$this->SendDataToChildren(json_encode(Array("DataID" => "{683476B1-08B4-682B-EF58-B892247FC2E1}", "Buffer" => $Text)));
 		}
+		
+		    /**
+     * Read config
+     */
+    private function ReadConfig()
+    {
+        // get settings
+	$this->bhkw_ip = $this->ReadPropertyInteger('bhkw_ip');
+        $this->port = $this->ReadPropertyInteger('port');
+        
+    }
+		    public function GetConfigurationForParent()
+    {
+        // read config
+        $this->ReadConfig();
+
+        // return config
+        return json_encode([
+            'Port' => $this->port,
+            'Open' => true
+        ]);
+    }
 
 	}

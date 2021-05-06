123b9b81e29bfc658d9be1ddc0e3028d2d8c090f
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-06 12:59:05 +0200

    Update Regelung.php

--- libs/Regelung.php
+++ libs/Regelung.php
@@ -4,30 +4,26 @@ declare(strict_types=1);
 
 trait CommonFunctions
 {
-public function VorlaufSoll()
-{
-
-	$time = date("H:i");			
-	//$VorlaufSoll = GetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"));
-	$AussenTemp = GetValueFloat($this->GetIDForIdent("T1"));
-	$VorlaufTempDiff = 70 - 45;
-	$VorlaufTempStep = $VorlaufTempDiff/40;
-	$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
-	//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
-	if(($time >= "22:30")or($time <= "05:00"))
+	public function VorlaufSoll()
 	{
-		$VorlaufSoll = $VorlaufSoll - 5;
+
+		$time = date("H:i");			
+		//$VorlaufSoll = GetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"));
+		$AussenTemp = GetValueFloat($this->GetIDForIdent("T1"));
+		$VorlaufTempDiff = 70 - 45;
+		$VorlaufTempStep = $VorlaufTempDiff/40;
+		$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
+		//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
+		if(($time >= "22:30")or($time <= "05:00"))
+		{
+			$VorlaufSoll = $VorlaufSoll - 5;
+		}
+		//If($AußenTemperatur > 3)
+		//{
+    		//}
+		IPS_LogMessage("$VorlaufTempStep",$VorlaufTempStep);
+		IPS_LogMessage("AußentemperaturT",$VorlaufSoll);
+		IPS_LogMessage("Außentemperatur", $this->GetIDForIdent("T1"));
+		SetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"), $VorlaufSoll);
 	}
-	//If($AußenTemperatur > 3)
-	//{
-    	//	If(($time >= "22:30")or($time <= "05:00"))
-    	//	{
-        //$VorlaufSoll = $VorlaufSoll - 5;
-    	//	}
-	//}
-	IPS_LogMessage("$VorlaufTempStep",$VorlaufTempStep);
-	IPS_LogMessage("AußentemperaturT",$VorlaufSoll);
-	IPS_LogMessage("Außentemperatur", $this->GetIDForIdent("T1"));
-	SetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"), $VorlaufSoll);
-}
 }

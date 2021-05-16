2a404785227406544699731a1fd1518bf2f22ae5
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-16 14:29:31 +0200

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -4,32 +4,31 @@ declare(strict_types=1);
 
 trait BHKWZustzHeizung
 {	
-private function ZusatzHeizung()
+	private function ZusatzHeizung()
+	{
+		$VorlaufSoll = GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"));
+		//IPS_LogMessage("zHeizung VorlaufSoll:", $VorlaufSoll);
+		$VorlaufMitteAus = $VorlaufSoll +8;
+		$SPmitte = GetValue($this->GetIDForIdent("T3"));
+		//IPS_LogMessage("zHeizung SPmitte:", $SPmitte);
+	/*	$VorlaufIst = GetValue($this->GetIDForIdent("T5"));
+		IPS_LogMessage("zHeizung VorlaufIst:", $VorlaufIst);
+		$HKPumpe = GetValue($this->GetIDForIdent("R1"));
+		IPS_LogMessage("zHeizung HKPumpe:", $HKPumpe);	*/
+		if(GetValue($this->GetIDForIdent("R1")))
 		{
-			$VorlaufSoll = GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"));
-			//IPS_LogMessage("zHeizung VorlaufSoll:", $VorlaufSoll);
-			$VorlaufMitteAus = $VorlaufSoll +8;
-			$SPmitte = GetValue($this->GetIDForIdent("T3"));
-			//IPS_LogMessage("zHeizung SPmitte:", $SPmitte);
-	/*		$VorlaufIst = GetValue($this->GetIDForIdent("T5"));
-			IPS_LogMessage("zHeizung VorlaufIst:", $VorlaufIst);
-			$HKPumpe = GetValue($this->GetIDForIdent("R1"));
-			IPS_LogMessage("zHeizung HKPumpe:", $HKPumpe);	*/
-			if(GetValue($this->GetIDForIdent("R1")))
+			// Heizung ist an
+			if(GetValue($this->GetIDForIdent("T5")) < GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"))-8)
 			{
-				// Heizung ist an
-				if(GetValue($this->GetIDForIdent("T5")) < GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"))-8)
-				{
-					SetValue($this->GetIDForIdent("zH1"), true);
-					IPS_LogMessage("zHeizung Heizung SP oben:",GetValue($this->GetIDForIdent("T5")));
-				}
-				//(GetValue($this->GetIDForIdent("T3")) > 70) or 
-				if($SPmitte > $VorlaufMitteAus)
-				{
-					SetValue($this->GetIDForIdent("zH1"), false);
-					IPS_LogMessage("zHeizung Heizung mitte:",GetValue($this->GetIDForIdent("T3")));
-					IPS_LogMessage("zHeizung Heizung vor+8:",$VorlaufSoll+8);
-				}
+				SetValue($this->GetIDForIdent("zH1"), true);
+				IPS_LogMessage("zHeizung Heizung SP oben:",GetValue($this->GetIDForIdent("T5")));
+			}
+			//(GetValue($this->GetIDForIdent("T3")) > 70) or 
+			if($SPmitte > $VorlaufMitteAus)
+			{
+				SetValue($this->GetIDForIdent("zH1"), false);
+				IPS_LogMessage("zHeizung Heizung mitte:",GetValue($this->GetIDForIdent("T3")));
+				IPS_LogMessage("zHeizung Heizung vor+8:",$VorlaufSoll+8);
 			}
 			else
 			{

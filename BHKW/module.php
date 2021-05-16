d232942db469aa77d34ae16cea49da1e334f44ff
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-16 14:13:15 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -2,6 +2,8 @@
 // include autoloader
 define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__ . '/libs/BHKW_Functions.php');
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__ . '/libs/BHKW_ZusatzHeizung.php');
 //require_once __DIR__ . '/../libs/BHKW_Functions.php';
 //const TempDiff =40;
 //const VorlaufSoll20 = 45;
@@ -18,7 +20,9 @@ if (!defined('VorlaufSollminus20')) {
 
 	class BHKW extends IPSModule {
 	
-		use BHKWFunctions;
+		use BHKWFunctions,
+		BHKWZusatzHeizung;
+		
 		public function Create()
 		{
 			//Never delete this line!

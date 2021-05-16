b0ec2c90622255a62042ea636004bed2b33b3d8e
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-16 14:10:28 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -670,52 +670,6 @@ if (!defined('VorlaufSollminus20')) {
 			return $ID;
 		} 
 		
-		private function ZusatzHeizung()
-		{
-			$VorlaufSoll = GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"));
-			//IPS_LogMessage("zHeizung VorlaufSoll:", $VorlaufSoll);
-			$VorlaufMitteAus = $VorlaufSoll +8;
-			$SPmitte = GetValue($this->GetIDForIdent("T3"));
-			//IPS_LogMessage("zHeizung SPmitte:", $SPmitte);
-	/*		$VorlaufIst = GetValue($this->GetIDForIdent("T5"));
-			IPS_LogMessage("zHeizung VorlaufIst:", $VorlaufIst);
-			$HKPumpe = GetValue($this->GetIDForIdent("R1"));
-			IPS_LogMessage("zHeizung HKPumpe:", $HKPumpe);	*/
-			if(GetValue($this->GetIDForIdent("R1")))
-			{
-				// Heizung ist an
-				if(GetValue($this->GetIDForIdent("T5")) < GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"))-8)
-				{
-					SetValue($this->GetIDForIdent("zH1"), true);
-					IPS_LogMessage("zHeizung Heizung SP oben:",GetValue($this->GetIDForIdent("T5")));
-				}
-				//(GetValue($this->GetIDForIdent("T3")) > 70) or 
-				if($SPmitte > $VorlaufMitteAus)
-				{
-					SetValue($this->GetIDForIdent("zH1"), false);
-					IPS_LogMessage("zHeizung Heizung mitte:",GetValue($this->GetIDForIdent("T3")));
-					IPS_LogMessage("zHeizung Heizung vor+8:",$VorlaufSoll+8);
-				}
-			}
-			else
-			{
-				// Heizung is aus (Warmwasser)
-				// Speichertemperatur oben > 65 zusatzHeizung aus
-				if (GetValue($this->GetIDForIdent("T2")) > 65)
-				{
-					SetValue($this->GetIDForIdent("zH1"), false);
-					IPS_LogMessage("zHeizung WWaus:",GetValue($this->GetIDForIdent("T2")));	
-				}
-				// Speichertemperatur oben < 55 zusatzHeizung an
-				if (GetValue($this->GetIDForIdent("T2")) < 55)
-				{
-					SetValue($this->GetIDForIdent("zH1"), true);
-					IPS_LogMessage("zHeizung WWan:",GetValue($this->GetIDForIdent("T2")));
-				}
-			}
-			return;						     
-		}
-		
 		private function IPS_CreateVariableProfile($ProfileName, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon) 
 		{
 		    if (!IPS_VariableProfileExists($ProfileName)) 

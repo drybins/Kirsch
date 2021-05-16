8b617037a7985187e6f7e5c265c65ba906545083
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-16 14:27:22 +0200

    Update BHKW_Functions.php

--- libs/BHKW_Functions.php
+++ libs/BHKW_Functions.php
@@ -2,28 +2,28 @@
 
 declare(strict_types=1);
 
-trait CommonFunctions
+trait BHKWFunctions
 {
-	public function VS()
+	public function VorlaufSoll()
 	{
 
-		//$time = date("H:i");			
+		$time = date("H:i");			
 		//$VorlaufSoll = GetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"));
-		//$AussenTemp = GetValueFloat($this->GetIDForIdent("T1"));
-		//$VorlaufTempDiff = 70 - 45;
-		//$VorlaufTempStep = $VorlaufTempDiff/40;
-		//$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
+		$AussenTemp = GetValueFloat($this->GetIDForIdent("T1"));
+		$VorlaufTempDiff = 70 - 45;
+		$VorlaufTempStep = $VorlaufTempDiff/40;
+		$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
 		//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
-		//if(($time >= "22:30")or($time <= "05:00"))
-		//{
-		//	$VorlaufSoll = $VorlaufSoll - 5;
-		//}
+		if(($time >= "22:30")or($time <= "05:00"))
+		{
+			$VorlaufSoll = $VorlaufSoll - 5;
+		}
 		//If($AußenTemperatur > 3)
 		//{
     		//}
-		IPS_LogMessage("VS:"," running");
-		//IPS_LogMessage("AußentemperaturT",$VorlaufSoll);
-		//IPS_LogMessage("Außentemperatur", $this->GetIDForIdent("T1"));
-		//SetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"), $VorlaufSoll);
+		IPS_LogMessage("$VorlaufTempStep",$VorlaufTempStep);
+		IPS_LogMessage("AußentemperaturT",$VorlaufSoll);
+		IPS_LogMessage("Außentemperatur", $this->GetIDForIdent("T1"));
+		SetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"), $VorlaufSoll);
 	}
 }

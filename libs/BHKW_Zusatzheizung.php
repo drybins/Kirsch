8b832bb776b44d5018000082b4bdaad8d664d7ce
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-05-17 09:42:35 +0200

    Add files via upload

---
+++ libs/BHKW_Zusatzheizung.php
@@ -0,0 +1,63 @@
+<?php
+/*****************************************************************************/
+/* erweitert das Kirsch BHKW Modul um dei möglichkeit eine Zusatheizung      */
+/* zu schalten.                                                              */
+/* Autohr:   D.Rybinski, KAT-CS                                              */
+/* Version:  1.0                                                             */
+/* Erstellt: 16.05.2021                                                      */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*****************************************************************************/
+
+declare(strict_types=1);
+
+trait BHKWZusatzHeizung
+{	
+	private function ZusatzHeizung()
+	{
+		$VorlaufSoll = GetValue($this->GetIDForIdent("VorlaufTemperaturSoll"));
+		$VorlaufIst = GetValue($this->GetIDForIdent("T5"));
+		$SPmitte = GetValue($this->GetIDForIdent("T3"));
+		//IPS_LogMessage("zHeizung VorlaufSoll:", $VorlaufSoll);
+		$VorlaufMitteAus = $VorlaufSoll + 8;
+		$VorlaufSollAn = $VorlaufSoll-8;
+		//IPS_LogMessage("zHeizung SPmitte:", $SPmitte);
+	 /*	IPS_LogMessage("zHeizung VorlaufIst:", $VorlaufIst);
+		$HKPumpe = GetValue($this->GetIDForIdent("R1"));
+		IPS_LogMessage("zHeizung HKPumpe:", $HKPumpe);	*/
+		if(GetValue($this->GetIDForIdent("R1")))
+		{
+			// Heizung ist an
+			if($VorlaufIst < $VorlaufSollAn and $SPmitte < $VorlaufSoll)
+			{
+				SetValue($this->GetIDForIdent("zH1"), true);
+				IPS_LogMessage("zHeizung Heizung SP oben:",GetValue($this->GetIDForIdent("T5")));
+			}
+			if($SPmitte > $VorlaufMitteAus)
+			{
+				SetValue($this->GetIDForIdent("zH1"), false);
+				IPS_LogMessage("zHeizung Heizung mitte:",GetValue($this->GetIDForIdent("T3")));
+				IPS_LogMessage("zHeizung Heizung vor+8:",$VorlaufMitteAus);
+			}
+		}
+		else
+		{
+			{
+				// Heizung is aus (Warmwasser)
+				// Speichertemperatur oben > 65 zusatzHeizung aus
+				if (GetValue($this->GetIDForIdent("T2")) > 65)
+				{
+					SetValue($this->GetIDForIdent("zH1"), false);
+					IPS_LogMessage("zHeizung WWaus:",GetValue($this->GetIDForIdent("T2")));	
+				}
+				// Speichertemperatur oben < 55 zusatzHeizung an
+				if (GetValue($this->GetIDForIdent("T2")) < 55)
+				{
+					SetValue($this->GetIDForIdent("zH1"), true);
+					IPS_LogMessage("zHeizung WWan:",GetValue($this->GetIDForIdent("T2")));
+				}
+			}
+		}
+	}
+}

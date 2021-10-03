4b4e3134257907b089742f3d061cbd5a50cee9fe
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-10-03 11:08:38 +0200

    Update BHKW_Functions.php

--- libs/BHKW_Functions.php
+++ libs/BHKW_Functions.php
@@ -10,9 +10,9 @@ trait BHKWFunctions
 		$time = date("H:i");			
 		//$VorlaufSoll = GetValueFloat($this->GetIDForIdent("VorlaufTemperaturSoll"));
 		$AussenTemp = GetValueFloat($this->GetIDForIdent("T1"));
-		$VorlaufTempDiff = 70 - 45;
+		$VorlaufTempDiff = 70 - 20;
 		$VorlaufTempStep = $VorlaufTempDiff/40;
-		$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 45;
+		$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 20;
 		//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
 		if(($time >= "22:30")or($time <= "05:00"))
 		{

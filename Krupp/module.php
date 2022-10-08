bf866d9f37f93f3f48a0ddb84a59cfb64c941d79
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-08 18:09:21 +0200

    Update module.php

--- Krupp/module.php
+++ Krupp/module.php
@@ -9,5 +9,16 @@ class Krupp extends IPSModule
         //You cannot use variables here. Just static values.
         $this->RegisterPropertyInteger("AN_Aus_Schalter", 0);
 	}
+	    public function Destroy()
+	{
+        //Never delete this line!
+        parent::Destroy();
+    }
+	
+    public function ApplyChanges()
+	{
+        //Never delete this line!
+        parent::ApplyChanges(); 
+    }
 }
 ?>

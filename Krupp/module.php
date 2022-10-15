f1db3e11b9d3e5e7dcdae55b3473e72fef0cff44
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-15 21:52:21 +0200

    Update module.php

--- Krupp/module.php
+++ Krupp/module.php
@@ -7,9 +7,14 @@ class KRUPP extends IPSModule
         parent::Create();
         //These lines are parsed on Symcon Startup or Instance creation 
         //You cannot use variables here. Just static values.
-        $this->RegisterPropertyInteger("AN_Aus_Schalter", 0);
+        $this->RegisterPropertyInteger("Hauptschalter", 0);
+		$this->RegisterPropertyInteger("Brenner_Schalter", 0);
+		$this->RegisterPropertyInteger("Pumpen_Schalter", 0);
 		
+		$this->RegisterVariableBoolean("Hauptschalter", "Hauptschalter", "Kirsch.AnAus", 0);
 		$this->RegisterVariableBoolean("Automatik", "Automatik", "Kirsch.AnAus", 10);
+		$this->RegisterVariableBoolean("Brenner", "Brenner", "Kirsch.AnAus", 20);
+		$this->RegisterVariableBoolean("Punmpe", "Pumpe", "Kirsch.AnAus", 30);
 	}
 	    public function Destroy()
 	{

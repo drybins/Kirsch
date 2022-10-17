e31c20414efa07b82745f094996cfbd52f27398a
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-17 21:15:54 +0200

    Update module.php

--- Krupp/module.php
+++ Krupp/module.php
@@ -11,11 +11,14 @@ class KRUPP extends IPSModule
 		$this->RegisterPropertyInteger("Brenner_Schalter", 0);
 		$this->RegisterPropertyInteger("Pumpen_Schalter", 0);
 		
+		$this->RegisterPropertyInteger("BHKW_Status", 0);
+		$this->RegisterPropertyInteger("Kessel_Anforderung", 0);
+		
 		$this->RegisterVariableBoolean("Hauptschalter", "Hauptschalter", "Kirsch.AnAus", 10);
 		$this->RegisterVariableBoolean("Holz_Oel", "Holz/Oel", "Kirsch.AnAus", 20);
 		$this->RegisterVariableBoolean("Automatik", "Automatik", "Kirsch.AnAus", 30);
 		$this->RegisterVariableBoolean("Brenner", "Brenner", "Kirsch.AnAus", 40);
-		$this->RegisterVariableBoolean("Punmpe", "Pumpe", "Kirsch.AnAus", 50);
+		$this->RegisterVariableBoolean("Pumpe", "Pumpe", "Kirsch.AnAus", 50);
 	}
 	    public function Destroy()
 	{

2d53edc2c70b5087de72c1bfe0389b2c2ba3e575
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-18 22:34:51 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -8,6 +8,7 @@ require_once(__ROOT__ . '/libs/BHKW_Zusatzheizung.php');
 require_once(__ROOT__ . '/libs/BHKW_statePP.php');
 //define('__ROOT3__', dirname(dirname(__FILE__)));
 require_once(__ROOT__ . '/libs/BHKW_statePower.php');
+require_once(__ROOT__ . '/libs/BHKW_errors.php');
 //const TempDiff =40;
 //const VorlaufSoll20 = 45;
 //const VorlaufSollminus20 = 70;
@@ -23,7 +24,7 @@ if (!defined('VorlaufSollminus20')) {
 
 	class BHKW extends IPSModule {
 	
-		use BHKWFunctions, BHKWZusatzHeizung, BHKWstatePP, BHKWstatePower;
+		use BHKWFunctions, BHKWZusatzHeizung, BHKWstatePP, BHKWstatePower, BHKWerrors;
 		
 		public function Create()
 		{
@@ -259,41 +260,6 @@ if (!defined('VorlaufSollminus20')) {
 				$this->ZusatzHeizung();
 			}
 		}
-		
-		private function errors($data)
-		{
-			//$arr = [];
-			$Datumalt =GetValue($this->GetIDForIdent("DLF"));
-			$array = json_decode(json_encode(simplexml_load_string($data)),true);
-			if(!empty($array))
-			{
-				//usort($array, function($a, $b)
-				//{
-				//	return new DateTime($a['datetime']) <=> new DateTime(b['datetime']);
-				//});
-				//print_r ($array);
-				//$sortarray = array();["error"]
-				//array_multisort( array_column( $array, 'date' ),SORT_DESC);
-				$i=0;
-				foreach($array['error'] as $elem)
-				{
-					$Datum = strtotime($elem['date'] . $elem['time']);
-					if($Datumalt == 0)
-					{
-						$Datumalt = $Datum;
-					}
-					if($Datum > $Datumalt)
-					{
-						SetValue($this->GetIDForIdent("DLF"),$Datum);
-						$Datumalt = $Datum;
-						IPS_LogMessage("BHKW Fehler datum:", $elem['date']);
-						IPS_LogMessage("BHKW Fehler Time:", $elem['time']);
-					}
-				}
-			}
-			IPS_LogMessage("BHKW errors:", $data);
-		}
-		
 		
 		/**
      		* Interne Funktion des SDK.

d7d26fb4ef82027da789511c1fe0b1f1013aacb8
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-18 21:25:14 +0200

    Create BHKW_statePower.php

---
+++ libs/BHKW_statePower.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+trait BHKWstatePower
+{
+	private function WriteLog1($data)
+	{
+		IPS_LogMessage("BHKW statePower TestLog", $data);
+	}
+	
+	private function statePower($data)
+	{
+		try
+		{
+			$xmlData = @new SimpleXMLElement(utf8_encode($data), LIBXML_NOBLANKS + LIBXML_NONET);
+			//echo "Alles ok!";
+		}
+ 			catch(Exception $ex)
+		{
+			//print_r($ex);
+			IPS_LogMessage("BHKW statePP Fehler", $data);
+		}
+		
+		//Status des BHKW'S
+		$ScriptData['STATUS'] = (string) $xmlData->state;			
+		//$StatusID = $this->GetIDForIdent("KirschStatus");
+		IPS_LogMessage("BHKW statePower Status", $ScriptData['STATUS']);
+		
+	}
+}

160e0a53eaed9eea77548419ea58528f90df7149
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-22 22:12:16 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -70,9 +70,9 @@ require_once(__ROOT__ . '/libs/BHKW_valuePP.php');
 			//$Parent = @IPS_GetCategoryIDByName("BHKW" , 0);
 			//$Parent = $this->GetParentId();
 			//print_r(IPS_GetInstance($Parent));
-			$ID = IPS_GetInstanceIDByName ("Kirsch BHKW Nano 4.12", 0);
-			IPS_LogMessage("BHKW ID1", $ID);
-			/*b
+			//$ID = IPS_GetInstanceIDByName ("Kirsch BHKW Nano 4.12", 0);
+			//IPS_LogMessage("BHKW ID1", $ID);
+			/*
 			$GUID = "{13D080B9-10DD-1AAD-4C21-B06937CDCA3C}";
 			$ID = IPS_GetInstanceListByModuleID($GUID)[0];
 			IPS_LogMessage("BHKW ID1", $ID);

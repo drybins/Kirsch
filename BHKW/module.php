d4d692ad72d8e2719deb0a85864a5db34e2c25e9
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-22 22:00:22 +0200

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -69,6 +69,7 @@ require_once(__ROOT__ . '/libs/BHKW_valuePP.php');
 			
 			//$Parent = @IPS_GetCategoryIDByName("BHKW" , 0);
 			$Parent = $this->GetParentId();
+			print_r(IPS_GetInstance($Parent));
 			IPS_LogMessage("BHKW ID1", $Parent);
 			/*b
 			$GUID = "{13D080B9-10DD-1AAD-4C21-B06937CDCA3C}";

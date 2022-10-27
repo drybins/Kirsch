09edff969b00987908820f28c62d0a5c0df18a8c
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2022-10-27 23:09:36 +0200

    Update BHKW_stateHeatControl.php

--- libs/BHKW_stateHeatControl.php
+++ libs/BHKW_stateHeatControl.php
@@ -78,7 +78,7 @@ trait BHKWstateHeatControl
 
 		$CPreset = $ScriptData['HKCP'] - 1;
 		$ScriptData['HKTKMin'] = (string) $xmlData->heatCircuits->heatCircuit->presets->preset[$CPreset];
-		IPS_LogMessage("BHKW stateHeatControl Kategorie", $$ScriptData['HKTKMin']);
+		IPS_LogMessage("BHKW stateHeatControl Kategorie", $ScriptData['HKTKMin']);
 		SetValueInteger(IPS_GetVariableIDByName("TKFlowMin", $KategorieID), $ScriptData['HKTKMin']);
 		
 		$KategorieID1 = @IPS_GetCategoryIDByName("Nachtabsenkung", $KategorieID);

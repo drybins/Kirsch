57bfe8955ce71cd02bc91a28f3527781711d83be
drybins <dierk@Rybinski-edv.de>
2022-12-01 05:57:34 +0100

    Update BHKW_Functions.php

--- libs/BHKW_Functions.php
+++ libs/BHKW_Functions.php
@@ -14,7 +14,7 @@ trait BHKWFunctions
 		$VorlaufTempStep = $VorlaufTempDiff/40;
 		$VorlaufSoll = ((20-$AussenTemp)* $VorlaufTempStep) + 38;
 		//Nachtabsenkung bei mehr als 3 Grad AußenTemperatur und zwischen 22:30 und 05:00 Uhr.
-		if(($time >= "22:30")or($time <= "05:00"))
+		if(($time >= "22:30")or($time <= "05:30"))
 		{
 			$VorlaufSoll = $VorlaufSoll - 5;
 		}

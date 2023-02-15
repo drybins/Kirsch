3b43fd48e7b45cde5f0df40883fbc64e8e3e70ce
dierkrybinski <dierkrybinski@users.noreply.github.com>
2023-02-15 01:24:19 +0100

    Create BHKW_stateExternal.php

---
+++ libs/BHKW_stateExternal.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+trait BHKWstatePower
+{
+	private function WriteLog1($data)
+	{
+		IPS_LogMessage("BHKW stateExternal TestLog", $data);
+	}
+	
+	private function stateExternal($data)
+	{
+		try
+		{
+			$xmlData = @new SimpleXMLElement(utf8_encode($data), LIBXML_NOBLANKS + LIBXML_NONET);
+			//echo "Alles ok!";
+		}
+ 			catch(Exception $ex)
+		{
+			//print_r($ex);
+			IPS_LogMessage("BHKW stateExternal Fehler", $data);
+		}
+		
+		$ScriptData['I1'] = (Float) $xmlData->I1;
+		SetValue ($this->GetIDForIdent("I1") , $ScriptData['I1']);
+
+	}
+}

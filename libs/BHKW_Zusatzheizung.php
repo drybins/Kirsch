c3a850d67635e95338a3fb445c236b80049c7fe4
dierkrybinski <dierkrybinski@users.noreply.github.com>
2023-10-31 14:23:00 +0100

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -20,7 +20,7 @@ trait BHKWZusatzHeizung
 		$VorlaufIst = GetValue($this->GetIDForIdent("T5"));
 		$SPOben = GetValue($this->GetIDForIdent("T2"));
 		$SPMitte = GetValue($this->GetIDForIdent("T3"));
-		$Heißwasser = GetValue($this->GetIDForIdent("WWTTarget"));
+		//$Heißwasser = GetValue($this->GetIDForIdent("WWTTarget"));
 		$WarmwasserStart = GetValue($this->GetIDForIdent("WWStartZeit"));
 		$WarmwasserEnde = GetValue($this->GetIDForIdent("WWEndeZeit"));
 		

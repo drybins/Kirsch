483259bfa60df478063962578845fea5dc361b3a
dierkrybinski <dierkrybinski@users.noreply.github.com>
2023-11-18 09:41:09 +0100

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -45,7 +45,7 @@ trait BHKWZusatzHeizung
         $Dauer = ($Jetzt - $SchaltZeit)/60;
 		IPS_LogMessage("zHeizung", "Dauer: " . $Dauer);
 		
-		if($Dauer > 300)
+		if($Dauer > 30)
         {
 			IPS_LogMessage("zHeizung", "Schalten");
 

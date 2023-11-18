aaa12243bea561b80431b70390d06b2f1dded02f
dierkrybinski <dierkrybinski@users.noreply.github.com>
2023-11-18 08:25:53 +0100

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -35,6 +35,11 @@ trait BHKWZusatzHeizung
 		$HKPumpe = GetValue($this->GetIDForIdent("R1"));
 		IPS_LogMessage("zHeizung","HKPumpe: " . $HKPumpe);	
 		$ZHH = false;
+		
+		$VarInfo = IPS_GetVariable($ZHID);
+		$EndeZeit = $VarInfo[VariableChanged]
+		Echo "Schaltzeit = " . $EndeZeit;
+		
 		if($HKPumpe)
 		{
 			IPS_LogMessage("zHeizung", "Heizkreispumpe ist an:");
@@ -95,8 +100,9 @@ trait BHKWZusatzHeizung
 				if(HM_RequestStatus($ZHID, "STATE") === true)
 				{
 					$RC = HM_WriteValueBoolean($ZHID, "STATE" , False);
+					echo "Heizung auß";
 				}
-				echo "Heizung auß";
+				
 			//}
 		}
 	}

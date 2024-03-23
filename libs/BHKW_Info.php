5f739854e60c82d942ae8f91026cfa2d8ec4349f
dierkrybinski <dierkrybinski@users.noreply.github.com>
2024-03-23 22:05:23 +0100

    Update BHKW_Info.php

--- libs/BHKW_Info.php
+++ libs/BHKW_Info.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-trait BHKWstatePower
+trait BHKWInfo
 {
-	private function WriteLog1($data)
+	private function WriteLog11($data)
 	{
-		IPS_LogMessage("BHKW statePower TestLog", $data);
+		IPS_LogMessage("BHKW Info TestLog", $data);
 	}
 	
-	private function statePower($data)
+	private function Info($data)
 	{
 		try
 		{
@@ -19,126 +19,13 @@ trait BHKWstatePower
  			catch(Exception $ex)
 		{
 			//print_r($ex);
-			IPS_LogMessage("BHKW statePower Fehler", $data);
+			IPS_LogMessage("BHKW Info Fehler", $data);
 		}
+			$GUID = "{13D080B9-10DD-1AAD-4C21-B06937CDCA3C}";
+			$ID = IPS_GetInstanceListByModuleID($GUID)[0];
 		
-		//Status des BHKW'S
-		$ScriptData['STATUS'] = (string) $xmlData->state;			
-		$StatusID = $this->GetIDForIdent("KirschStatus");
-		//IPS_LogMessage("BHKW statePower Status", $ScriptData['STATUS']);
-				
-		switch ($ScriptData['STATUS']) 
-		{
-			case "stop":
-			if(GetValue($StatusID) === 13)
-				SetValueInteger ($StatusID, 11);
-			else
-				SetValueInteger ($StatusID, 1);
-			break;
-		case "start":
-			SetValueInteger ($StatusID, 2);
-			break;
-		case "warmup":
-			SetValueInteger ($StatusID, 3);
-			break;
-		case "running":
-			SetValueInteger ($StatusID, 4);
-			break;
-		case "cooldown":
-			SetValueInteger ($StatusID, 5);
-			break;
-		case "selftest":
-			if(GetValue($StatusID) === 11)
-					SetValueInteger ($StatusID, 13);
-			else
-				SetValueInteger ($StatusID, 6);
-			break;
-		case "oelrefill":
-			SetValueInteger ($StatusID, 7);
-			break;
-
-		case "emergencystop":
-			SetValueInteger ($StatusID, 10);
-			break;  
-		case "error":
-			SetValueInteger ($StatusID, 11);
-			break;         
-		default:
-			//SetValueString (14320 , "Status nicht gefunden:" . $ScriptData['STATUS']);
-		}
-		
-		//Status des BHKW'S
-//		$ScriptData['zH1'] = (string) $xmlData->extStby;
-//		switch ($ScriptData['zH1']) 
-//		{
-//		case "on":
-//			SetValueBoolean($this->GetIDForIdent("zH1"), true);
-//			break;
-//		case "off":
-//			SetValueBoolean ($this->GetIDForIdent("zH1"), false);
-//			break;
-//		default:		
-//		}
-		
-		$ScriptData['ZielleistungDA'] = (string) $xmlData->powerClasses[0]['autoAdapt'];
-		//IPS_LogMessage("BHKW statePower Autoadapt", $ScriptData['ZielleistungDA']);
-		switch ($ScriptData['ZielleistungDA']) 
-		{
-		case "enabled":
-			SetValueBoolean($this->GetIDForIdent("ZielleistungDA"), true);
-			break;
-		case "disabled":
-			SetValueBoolean ($this->GetIDForIdent("ZielleistungDA"), false);
-			break;
-		default:
-			//SetValueString (14320 , "Status nicht gefunden:" . $ScriptData['STATUS']);
-		}
-		
-		for ($x = 0; $x <= 5; $x+=1) 
-		{
-			$ScriptData['T'] = (string) $xmlData->temperatures->temperature[$x];
-			//IPS_LogMessage("BHKW statePower time", $x . " : " . $ScriptData['T']);
-			switch ($x) 
-			{
-				case "0":
-					SetValueFloat($this->GetIDForIdent("TI3") , $ScriptData['T']);
-					//IPS_LogMessage("BHKW statePower Ti1", $x . " : " . $ScriptData['T']);
-				break;
-				case "1":
-					SetValueFloat($this->GetIDForIdent("TI2") , $ScriptData['T']);
-					//IPS_LogMessage("BHKW statePower Ti2", $x . " : " . $ScriptData['T']);
-				break;
-				case "3":
-					SetValueFloat($this->GetIDForIdent("TI4") , $ScriptData['T']);
-					//IPS_LogMessage("BHKW statePower Ti4", $x . " : " . $ScriptData['T']);
-				break;
-				case "4":
-					SetValueFloat($this->GetIDForIdent("TI5") , $ScriptData['T']);
-					//IPS_LogMessage("BHKW statePower Ti5", $x . " : " . $ScriptData['T']);
-				break;
-				case "5":
-					SetValueFloat($this->GetIDForIdent("TI6") , $ScriptData['T']);
-					//IPS_LogMessage("BHKW statePower Ti6", $x . " : " . $ScriptData['T']);
-				break;
-			default:
-				//;
-			} 
-		}
-		for ($x = 0; $x <= 23; $x+=1) 
-		{
-	
-			$ScriptData['ZielleistungT'] = (string) $xmlData->powerClasses->targetPower[$x]['time'];
-			$TargetTime = substr($ScriptData['ZielleistungT'],0,2);
-			//IPS_LogMessage("BHKW statePower time", $TargetTime);
-			//IPS_LogMessage("BHKW statePower x", $x);
-			$ScriptData['Zielleistung'] = (string) $xmlData->powerClasses->targetPower[$x];
-			//IPS_LogMessage("BHKW statePower Zielleistung", $ScriptData['Zielleistung']);
-			//IPS_LogMessage("BHKW statePower Zielleistung", $this->GetIDForIdent("Zielleistung" . $x));
-			if($x == $TargetTime)
-			{
-					//IPS_LogMessage("BHKW statePower time", "korreckt");
-					SetValueInteger($this->GetIDForIdent("Zielleistung" . $x),$ScriptData['Zielleistung']);
-			}
+			$KategorieID = @IPS_GetCategoryIDByName("Hardware", $ID);
+			$ScriptData['IP'] = (string) $xml->ip;
 
 		}
 	}

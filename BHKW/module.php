114f6db779c779c338510126f470df21fa8b56e8
dierkrybinski <dierkrybinski@users.noreply.github.com>
2024-03-26 19:33:15 +0100

    Update module.php

--- BHKW/module.php
+++ BHKW/module.php
@@ -306,10 +306,11 @@ require_once(__ROOT__ . '/libs/BHKW_CreateProfile.php');
 			$this->RegisterVariableFloat("T4", "Speichertemperatur unten", "~Temperature", 530);
 
 			$this->RegisterVariableBoolean("R1", "Heizkreispumpe", "Kirsch.AnAus", 532);
-			$this->RegisterVariableBoolean("R2", "Mischer auf", "Kirsch.AnAus", 533);
-			$this->RegisterVariableBoolean("R3", "Mischer zu", "Kirsch.AnAus", 534);
+			$this->RegisterVariableBoolean("Mischer", "Mischer Status", "", 533);
+			$this->RegisterVariableBoolean("R2", "Mischer auf", "Kirsch.AnAus", 534);
+			$this->RegisterVariableBoolean("R3", "Mischer zu", "Kirsch.AnAus", 535);
 			
-			$this->RegisterVariableFloat("VorlaufTemperaturSoll", "Vorlauf Temperatur soll", "~Temperature", 535);
+			$this->RegisterVariableFloat("VorlaufTemperaturSoll", "Vorlauf Temperatur soll", "~Temperature", 536);
 			$this->RegisterVariableFloat("T5", "Vorlauf Heizkreis 1", "~Temperature", 538);
 			$this->RegisterVariableFloat("T6", "Rücklauf Heizkreis 1", "~Temperature", 540);
 			$this->RegisterVariableBoolean("R4", "ZusatzHeizung", "Kirsch.AnAus", 542);

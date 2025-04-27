acc058a7ad50c79d4aa188b7db322dc41d390123
dierkrybinski <dierkrybinski@users.noreply.github.com>
2025-04-27 21:18:55 +0200

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -36,7 +36,7 @@ trait BHKWZusatzHeizung
 		$AID = IPS_GetObjectIDByName ("Archive", 0);
 		
 		$newDate = date('Y-m-d H:i:s', strtotime(' -5 minutes'));
-		$newDate1 = date('Y-m-d H:i:s', strtotime(' -1 minutes'));
+		$newDate1 = date('Y-m-d H:i:s', strtotime(' -0 minutes'));
 		$last_value = AC_GetLoggedValues($AID, $IdentVorlaufKrupp,  strtotime($newDate), strtotime($newDate1), 1)[0]['Value'];
 		$Vorlauf_Krupp = GetValue($IdentVorlaufKrupp);
 		//$

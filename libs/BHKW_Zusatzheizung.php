9438db6879c9c4efd54f397308577ec5457f0f79
dierkrybinski <dierkrybinski@users.noreply.github.com>
2025-05-13 22:23:09 +0200

    Update BHKW_Zusatzheizung.php

--- libs/BHKW_Zusatzheizung.php
+++ libs/BHKW_Zusatzheizung.php
@@ -195,7 +195,7 @@ trait BHKWZusatzHeizung
 	{
 	}
 	
-	private function PumpeSchalten(Bool $PumpeStatusSoll, int $SchellyID)
+	private function PumpeSchalten(int $PumpeStatusSoll, int $SchellyID)
 	{
 			$IdentKruppPumpe = IPS_GetObjectIDByIdent("KruppPumpe",$SchellyID);
 			IPS_LogMessage("zHeizungH","IdentKruppPumpe: " . $IdentKruppPumpe);
